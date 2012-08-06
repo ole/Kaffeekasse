f16e019ff80b4e441157c0b2fb64dddbb8177415
Ole Begemann <ole@oleb.net>
2012-08-06 11:02:31 +0200

    Blank Xcode project

---
+++ Kaffeekasse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Kaffeekasse
+//
+//  Created by Ole Begemann on 06.08.12.
+//  Copyright (c) 2012 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Kaffeekasse/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Kaffeekasse
+//
+//  Created by Ole Begemann on 06.08.12.
+//  Copyright (c) 2012 Ole Begemann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
